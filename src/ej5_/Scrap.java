24784f1a0f0130d4b1f58f7a26c5fd5ba0a684ab
dam203 <dam203@PC2528.aulas.local>
2018-02-07 14:40:10 +0100

    Añadido ejercicio 5

---
+++ src/ej5_/Scrap.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ej5_;
+
+import java.io.IOException;
+import java.util.Observable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+/**
+ *
+ * @author dam203
+ */
+public class Scrap extends Observable implements Runnable {
+
+    @Override
+    public void run() {
+        String url = "https://elpais.com/";
+        System.out.println("Fetching %s..."+ url);
+
+        Document doc;
+        String result="";
+        try {
+            doc = Jsoup.connect(url).get();
+            Elements tit = doc.getElementsByClass("articulo-titulo");
+            Elements titulares = tit.select("a");
+            
+            for(Element link: titulares){
+                result = result + "<br/><a href=\"https://elpais.com" + link.attr("href").toString()+"\">"+link.text()+ "<a/><br/>";
+                System.out.println("<br/><a href=\"https://elpais.com" + link.attr("href").toString()+">"+link.text()+ "<a/><br/>");
+            }
+            
+            this.setChanged();
+            this.notifyObservers(result);
+            this.clearChanged();
+            System.out.println("SCRAP DONE");
+        } catch (IOException ex) {
+            Logger.getLogger(Scrap.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
